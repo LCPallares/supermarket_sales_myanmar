f2bb913744cf97e8cd713f2c4f8967f6d0a3f613
LCPallares <poncho6257@gmail.com>
2026-01-05 17:15:08 -0500

    fucion de seccion metricas y graficos a ventas, top productos convertida en funcion

--- vizualization/streamlit/supermarket_sales_streamlit.py
+++ vizualization/streamlit/supermarket_sales_streamlit.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 from streamlit_folium import st_folium
 import folium
-import json
 
 # Configuración de la página
 st.set_page_config(page_title="Dashboard de Ventas de Supermercado de Myanmar", layout="wide")
@@ -87,6 +86,7 @@ def graficar_distribucion_precios_unitarios(df):
 
 def mostrar_metricas(df):
     col1, col2, col3, col4 = st.columns(4)
+    # para poner las metricas verticales remplazar col(n) por st
     col1.metric("Ventas Totales", f"${df['Total'].sum():,.2f}")
     col2.metric("Transacciones", f"{len(df):,}")
     col3.metric("Venta Promedio", f"${df['Total'].mean():,.2f}")
@@ -97,10 +97,37 @@ def mostrar_analisis_margen_bruto(df):
     margen_promedio = df['Porcentaje de Margen Bruto'].mean()
     st.write(f"El margen bruto promedio es: {margen_promedio:.2f}%")
 
+def mostrar_top_productos(filtered_df):
+    # Calcular top productos
+    top_products = filtered_df.groupby('Línea de Producto')['Cantidad'].sum().reset_index()
+    top_products = top_products.sort_values(by='Cantidad', ascending=False).head(5)
+    top_products.columns = ['producto', 'cantidad']
+    
+    # Convertir los datos a tipos nativos de Python
+    top_products['cantidad'] = top_products['cantidad'].astype(int).tolist()
+    
+    # Crear el dataframe con la configuración actualizada
+    st.dataframe(
+        top_products,
+        column_order=("producto", "cantidad"),
+        hide_index=True,
+        width=None,
+        column_config={
+            "producto": st.column_config.TextColumn(
+                "Producto",
+            ),
+            "cantidad": st.column_config.ProgressColumn(
+                "Cantidad",
+                format="%d",
+                min_value=0,
+                max_value=int(max(top_products['cantidad'])),
+            )}
+    )
+
 # Título y barra de navegación
 st.sidebar.header("Secciones")
 st.title("Dashboard de Ventas de Supermercado de Myanmar")
-nav = st.sidebar.radio("Navegar a", ["Métricas", "Gráficos", "Tabla de Datos"])
+nav = st.sidebar.radio("Navegar a", ["Ventas", "Tabla de Datos"])
 
 # Filtros en el sidebar
 st.sidebar.header("Filtros")
@@ -117,16 +144,12 @@ if productos:
     mask &= df['Línea de Producto'].isin(productos)
 filtered_df = df[mask]
 
-
-# Mostrar la sección seleccionada en la barra de navegación
-if nav == "Métricas":
+# Sección de ventas
+if nav == "Ventas":
     mostrar_metricas(filtered_df)
-    #graficar_metodos_de_pago(filtered_df)
-
-elif nav == "Gráficos":
     graficar_mapa_de_ventas(filtered_df)
-    col1, col2, col3 = st.columns(3)
-
+    col1, col2, col3 = st.columns((1.5, 4.5, 2), gap='medium')  # margenes de las columnas
+    
     with col1:
         graficar_ventas_diarias(filtered_df)
         graficar_ventas_por_tipo_cliente_y_genero(filtered_df)
@@ -136,50 +159,22 @@ elif nav == "Gráficos":
         graficar_ventas_por_linea_de_producto(filtered_df)
         graficar_metodos_de_pago(filtered_df)
         graficar_distribucion_precios_unitarios(filtered_df)
-    
+
     with col3:
         st.markdown('#### Top Productos')
-
-        top_products = filtered_df.groupby('Línea de Producto')['Cantidad'].sum().reset_index()
-        top_products = top_products.sort_values(by='Cantidad', ascending=False).head(5)
-        top_products.columns = ['producto', 'cantidad']
+        mostrar_top_productos(filtered_df)
         
-        # Convertir los datos a tipos nativos de Python
-        top_products['cantidad'] = top_products['cantidad'].astype(int).tolist()
-        
-        # Crear el dataframe con la configuración actualizada
-        st.dataframe(
-            top_products,
-            column_order=("producto", "cantidad"),
-            hide_index=True,
-            width=None,
-            column_config={
-                "producto": st.column_config.TextColumn(
-                    "Producto",
-                ),
-                "cantidad": st.column_config.ProgressColumn(
-                    "Cantidad",
-                    format="%d",
-                    min_value=0,
-                    max_value=int(max(top_products['cantidad'])),
-                )}
-        )
-    
         with st.expander('Acerca de', expanded=True):
             st.write('''
                 - Datos: [Datos de ventas de supermercado en Myanmar](ficticio-data-source.com).
                 - :orange[**Top Productos**]: productos más vendidos por cantidad para el periodo seleccionado.
                 - :orange[**Cantidad**]: número de unidades vendidas.
                 ''')
-    
+
+    mostrar_analisis_margen_bruto(filtered_df)
 
 elif nav == "Tabla de Datos":
     st.dataframe(filtered_df)
-    #graficar_metodos_de_pago(filtered_df)
 
 # Añadir más componentes
 st.markdown("---")
-
-#graficar_cantidad_de_productos(filtered_df)
-#graficar_distribucion_precios_unitarios(filtered_df)
-#mostrar_analisis_margen_bruto(filtered_df)
